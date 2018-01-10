e283407b15164b59d2b26c3109aa90312c891716
jerozerbib <jerozerbib@gmail.com>
2018-01-10 08:38:33 +0100

    Presque fini

--- main.cpp
+++ main.cpp
@@ -16,37 +16,115 @@
 #include <vector>
 #include <iostream>
 #include <cstdlib>
+#include <numeric>
+#include <algorithm>
 
 using namespace std;
 using Line   = vector<int>;
 using Matrix = vector<Line>;
 
-bool isSquared(Matrix& m);
+bool isFull     (const Matrix& m);
 
-Line lineSum(Matrix& m);
+bool isSquared  (const Matrix& m);
 
-Line colSum(Matrix& m);
+Line lineSum    (const Matrix& m);
 
-bool antiDiagSum(int& sum, Matrix& m);
+Line colSum     (const Matrix& m);
 
-bool diagSum(int& sum, Matrix& m);
+bool antiDiagSum(int& sum, const Matrix& m);
 
-void display(Matrix& m);
+bool diagSum    (int& sum, const Matrix& m);
 
-void display(Line& l);
+void display    (const Matrix& m);
 
-Line min(Matrix& m);
+void display    (const Line& l);
 
-void shuffle(Matrix& m);
+Line min        (const Matrix& m);
 
-void shuffle(Matrix& m, int line);
+void shuffle    (Matrix& m);
 
-void sort(Matrix& m);
+void shuffle    (Matrix& m, int line);
 
-void sort(Matrix& m, int line);
+void sort       (Matrix& m);
+
+void sort       (Matrix& m, int line);
 
 int main(){
+
+    Line l1 = {1, 2, 3};
+    Matrix m = {l1};
+
+    display(lineSum(m));
     return EXIT_SUCCESS;
+}
+bool isFull(const Matrix& m) {
+    const int& size = m.front().size();
+    return all_of(m.begin()+1, m.end(), [=](Line l){ return l.size() == size; });
+}
+
+bool isSquared(const Matrix& m) {
+    return isFull(m) && m.size() == m.front().size();
+}
+
+Line lineSum(const Matrix& m){
+    Line sumLine;
+    for (const Line& l : m) {
+        sumLine.push_back(accumulate(l.begin(), l.end(), 0));
+    }
+    return sumLine;
+}
+
+Line colSum(const Matrix& m){
+    Line maxLine;
+    maxLine = max_element(m.begin(), m.end(), [](Line a, Line b){ return a.size() < b.size();});
+}
+
+void display(const Matrix& m) {
+    cout << "[";
+    for (const Line& l : m) {
+        if (l != *m.begin()){
+            cout << ",";
+        }
+        display(l);
+    }
+    cout << "]";
 }
 
 
+void display(const Line& l) {
+    cout << "[";
+    for (auto i = l.begin(); i != l.end(); ++i) {
+        if (i != l.begin()) {
+            cout << ",";
+        }
+        cout << *i;
+    }
+    cout << "]";
+}
+
+
+Line min(const Matrix& m){
+    Line sum = lineSum(m);
+    int index;
+    auto min = min_element(sum.begin(), sum.end());
+    index = distance(sum.begin(), min);
+    return m.at(index);
+}
+
+
+void shuffle(Matrix& m){
+    random_shuffle(m.begin(), m.end());
+}
+
+void shuffle(Matrix& m, int line){
+    random_shuffle(m.at(line).begin(), m.at(line).end());
+}
+
+void sort(Matrix& m){
+    sort(m.begin(), m.end(), greater<int>());
+}
+
+
+void sort(Matrix& m, int line){
+    sort(m.at(line).begin(), m.at(line).end());
+}
