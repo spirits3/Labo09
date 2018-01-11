ad0a43ef1c0791d7d2383051c4ff8566c6d56d56
spirits3 <flo_diablo@hotmail.com>
2018-01-11 16:39:35 +0100

    add parameters and fix some function for out of bounds

--- matrix.cpp
+++ matrix.cpp
@@ -18,6 +18,11 @@
 
 using namespace std;
 
+/**
+ * check if the matrix is full
+ * @param  m matrix
+ * @return   bool
+ */
 bool isFull(const Matrix &m) {
     if (m.empty()) {
         return false;
@@ -26,10 +31,20 @@ bool isFull(const Matrix &m) {
     return all_of(m.begin() + 1, m.end(), [=](Line l) { return l.size() == size; });
 }
 
+/**
+ * check if the matrix is squarred
+ * @param  m matrix
+ * @return   bool
+ */
 bool isSquared(const Matrix &m) {
     return isFull(m) && m.size() == m.front().size();
 }
 
+/**
+ * vector with sum of value in each line of the matrix 
+ * @param  m matrix
+ * @return   Line (vector<int>)
+ */
 Line lineSum(const Matrix &m) {
     Line sumLine;
     for (const Line &l : m) {
@@ -38,6 +53,11 @@ Line lineSum(const Matrix &m) {
     return sumLine;
 }
 
+/**
+ * will check what is the maxCol of the matrix
+ * @param  m matrix
+ * @return   unsigned
+ */
 unsigned maxCol(const Matrix &m) {
 
     if (m.empty()) {
@@ -49,6 +69,11 @@ unsigned maxCol(const Matrix &m) {
     }))).size();
 }
 
+/**
+ * sum in each coloumn of the matrix
+ * @param  m matrix
+ * @return   Line (vector<int>)
+ */
 Line colSum(const Matrix &m) {
     Line retour(maxCol(m));
     for (const Line &l : m) {
@@ -59,6 +84,12 @@ Line colSum(const Matrix &m) {
     return retour;
 }
 
+/**
+ * sum of the antidiag of the squarred matrix
+ * @param  sum int sum that we will use outside the function
+ * @param  m   matrix
+ * @return     bool (check if the matrix is squarred)
+ */
 bool antiDiagSum(int &sum, const Matrix &m) {
     if (isSquared(m)) {
         for (int i = 0; i < m.size(); ++i) {
@@ -69,6 +100,12 @@ bool antiDiagSum(int &sum, const Matrix &m) {
     return false;
 }
 
+/**
+ * sum of the diagonal of the matrix
+ * @param  sum int sum that we will use outside the function
+ * @param  m   matrix
+ * @return     bool (check if the matrix is squarred)
+ */
 bool diagSum(int &sum, const Matrix &m) {
     if (isSquared(m)) {
         for (int i = 0; i < m.size(); ++i) {
@@ -79,6 +116,10 @@ bool diagSum(int &sum, const Matrix &m) {
     return false;
 }
 
+/**
+ * display the matrix
+ * @param m matrix
+ */
 void display(const Matrix &m) {
     cout << "[";
     for (const Line &l : m) {
@@ -92,6 +133,10 @@ void display(const Matrix &m) {
     JUMP;
 }
 
+/**
+ * display a vector
+ * @param l Line l (vector<int> l)
+ */
 void display(const Line &l) {
     cout << "[";
     for (Line::const_iterator i = l.begin(); i != l.end(); ++i) {
@@ -104,7 +149,11 @@ void display(const Line &l) {
 
 }
 
-
+/**
+ * check which sum of vector is the small one and will return the vector
+ * @param  m matrix
+ * @return   Line (vector<int>)
+ */
 Line min(const Matrix &m) {
     Line sum = lineSum(m);
     int index;
@@ -113,17 +162,31 @@ Line min(const Matrix &m) {
     return m.at(index);
 }
 
-
+/**
+ * shuffle the order of the vector in the matrix
+ * @param m matrix
+ */
 void shuffle(Matrix &m) {
     initRandom();
     random_shuffle(m.begin(), m.end());
 }
 
+/**
+ * shuffle a Line of the matrix
+ * @param m    matrix
+ * @param line line (vector<int>)
+ */
 void shuffle(Matrix &m, int line) {
-    initRandom();
-    random_shuffle(m.at(line).begin(), m.at(line).end());
+    if(line < m.size()){
+        initRandom();
+        random_shuffle(m.at(line).begin(), m.at(line).end());
+    }
 }
 
+/**
+ * will sort the matrix between the sum of line in ascending order
+ * @param m matrix
+ */
 void sort(Matrix &m) {
     for (auto it = m.begin(); it != m.end(); ++it) {
         sort(it, m.end(), [](Line l1, Line l2) {
@@ -132,7 +195,13 @@ void sort(Matrix &m) {
     }
 }
 
+/**
+ * will sort line in the matrix in ascending order
+ * @param m    matrix
+ * @param line Line (vector<int>)
+ */
 void sort(Matrix &m, int line) {
-    sort(m.at(line).begin(), m.at(line).end());
+    if(line < m.size())
+        sort(m.at(line).begin(), m.at(line).end());
 }
 
